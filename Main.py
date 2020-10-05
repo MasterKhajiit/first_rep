98dde3506b613fd0ad08ef13f1d655270eebdc2f
Wadim Rudkovski <wadim_rudkovski@yahoo.com>
2020-10-05 15:26:12 +0300

    Added a constructor for BaseIncome class Added method annual_income for BaseIncome class Added description for BaseIncome class Added description for annual_income method in BaseIncome class

--- Main.py
+++ Main.py
@@ -1,5 +1,21 @@
 class BaseIncome:
-    pass
+    """
+    Basic class include parameters for all groups of people
+
+    people_count - (int), number of people of specific group
+    average_income - (int), average income for 1 person of specific group in BYN
+    tax_rate - (int), tax rate in percent for specific group. Default value for most of groups is "13"
+
+    annual_income - (float) method returns annual value of taxes for one specific group
+    """
+
+    def __init__(self, people_count, average_income, tax_rate=13):
+        self.people_count = people_count  # set average people base
+        self.average_income = average_income  # set average income for 1 person
+        self.tax_rate = tax_rate / 100  # set tax rate for specific business branch
+
+    def annual_income(self):
+        return self.people_count * (self.average_income * self.tax_rate)
 
 
 class BaseExpenses:
