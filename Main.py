44c4689b289b350a5da717ad97f4ea2df8a5282b
Wadim Rudkovski <wadim_rudkovski@yahoo.com>
2020-10-05 16:00:01 +0300

    Modified a constructor for BaseIncome class Modified description for annual_income method in BaseIncome class

--- Main.py
+++ Main.py
@@ -1,22 +1,33 @@
 class BaseIncome:
     """
-    Basic class include parameters for all groups of people
+    Basic class include parameters of income for all groups of people
 
     people_count - (int), number of people of specific group
     average_income - (int), average income for 1 person of specific group in BYN
-    tax_rate - (int), tax rate in percent for specific group. Default value for most of groups is "13"
+    personal_tax_rate - (int), personal tax rate in percent for specific group. Default value for most of groups is "13"
+    organization_tax_rate - (int) organization tax rate in percent for specific group.Default value for most of groups is "34"
 
     annual_income - (float) method returns annual value of taxes for one specific group
     """
 
-    def __init__(self, people_count, average_income, tax_rate=13):
+    def __init__(self, people_count, average_income, personal_tax_rate=13, organization_tax_rate=34):
         self.people_count = people_count  # set average people base
         self.average_income = average_income  # set average income for 1 person
-        self.tax_rate = tax_rate / 100  # set tax rate for specific business branch
+        self.personal_tax_rate = personal_tax_rate / 100  # set personal tax rate for specific business branch
+        self.organization_tax_rate = organization_tax_rate  # set organization tax rate
 
     def annual_income(self):
-        return self.people_count * (self.average_income * self.tax_rate)
+        return self.people_count * (self.average_income * (self.personal_tax_rate + self.organization_tax_rate)
 
 
 class BaseExpenses:
-    pass
+    """
+    Basic class include parameters of expenses for all groups of people
+
+    people_count - (int), number of people of specific group
+    average_payment - (int), average expenses for 1 person of specific group in BYN
+    """
+
+    def __init__(self, people_count, average_payment):
+        self.people_count = people_count  # set average people base
+        self.average_payment = average_payment  # set average payment for 1 person
