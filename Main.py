52e42a2a1fc922b2eb3ee0dbd756256e90f2af6f
Wadim Rudkovski <wadim_rudkovski@yahoo.com>
2020-10-05 19:57:28 +0300

    Bug fix in organization_tax_rate PEP8 changes in BaseIncome dockstring

--- Main.py
+++ Main.py
@@ -5,7 +5,8 @@ class BaseIncome:
     people_count - (int), number of people of specific group
     average_income - (int), average income for 1 person of specific group in BYN
     personal_tax_rate - (int), personal tax rate in percent for specific group. Default value for most of groups is "13"
-    organization_tax_rate - (int) organization tax rate in percent for specific group.Default value for most of groups is "34"
+    organization_tax_rate - (int) organization tax rate in percent for specific group.Default value for most of groups
+    is "34"
 
     annual_income - (float) method returns annual value of taxes for one specific group
     """
@@ -14,10 +15,10 @@ class BaseIncome:
         self.people_count = people_count  # set average people base
         self.average_income = average_income  # set average income for 1 person
         self.personal_tax_rate = personal_tax_rate / 100  # set personal tax rate for specific business branch
-        self.organization_tax_rate = organization_tax_rate / 100 # set organization tax rate
+        self.organization_tax_rate = organization_tax_rate / 100  # set organization tax rate
 
     def annual_income(self):
-        return self.people_count * (self.average_income * (self.personal_tax_rate + self.organization_tax_rate)
+        return self.people_count * (self.average_income * (self.personal_tax_rate + self.organization_tax_rate))
 
 
 class BaseExpenses:
